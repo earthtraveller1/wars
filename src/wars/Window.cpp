a5705d73ef7f1ff3f46377484de496c4a6b91945
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-20 14:35:47 -0400

    Created a window class.

---
+++ src/wars/Window.cpp
@@ -0,0 +1,83 @@
+#include <stdexcept>
+
+#include "Window.hpp"
+
+static void initGLFW() {
+    if (!glfwInit()) {
+        throw std::runtime_error("[FATAL ERROR]: Failed to initialize GLFW.");
+    }
+}
+
+static void setWindowHints() {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+}
+
+void Window::create(bool fullscreen, std::string_view title) {
+    GLFWmonitor* monitor = nullptr;
+    
+    if (fullscreen) {
+        monitor = glfwGetPrimaryMonitor();
+    }
+    
+    window = glfwCreateWindow(width, height, title.data(), monitor, nullptr);
+    if (window == nullptr) {
+        glfwTerminate();
+        throw std::runtime_error("[FATAL ERROR]: Failed to create GLFW window.");
+    }
+    glfwMakeContextCurrent(window);
+}
+
+Window::Window(): Window(0.5, "GLFW Window", false) {
+    
+}
+
+Window::Window(unsigned int width, unsigned int height, std::string_view title, bool fullscreen) {
+    initGLFW();
+    setWindowHints();
+    
+    this->width = width;
+    this->height = height;
+    
+    create(fullscreen, title);
+}
+
+Window::Window(double multiplier, std::string_view title, bool fullscreen) {
+    initGLFW();
+    setWindowHints();
+    
+    const GLFWvidmode* videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    
+    this->width = videoMode->width * multiplier;
+    this->height = videoMode->height * multiplier;
+    
+    create(fullscreen, title);
+}
+
+void Window::show() {
+    glfwShowWindow(window);
+}
+
+bool Window::isOpen() {
+    return !glfwWindowShouldClose(window);
+}
+
+void Window::update() {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+unsigned int Window::getWidth() {
+    return width;
+}
+
+unsigned int Window::getHeight() {
+    return height;
+}
+
+Window::~Window() {
+    glfwDestroyWindow(window);
+}
