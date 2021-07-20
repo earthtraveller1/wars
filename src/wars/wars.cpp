7ad121a510c680b3532f6add82683f80b857df95
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-20 14:42:11 -0400

    Added a window.

--- src/wars/wars.cpp
+++ src/wars/wars.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Window.hpp"
 
 int main() {
     std::cout << "[INFO]: Hello!" << std::endl;
+    
+    Window window(1.0, "", true);
+    
+    window.show();
+    while (window.isOpen()) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        window.update();
+    }
+    
     return 0;
 }
