20a62c314d18a5f61f062ee6c996be3fbbdbc0db
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-20 13:55:54 -0400

    Added GLFW

---
+++ cmake/FindGLFW.cmake
@@ -0,0 +1,60 @@
+find_path(
+    GLFW_INCLUDE_DIR
+    NAMES "GLFW/glfw3.h"
+    PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/include"
+)
+
+if (MSVC)
+    if (USE_STATIC_LIBS)
+        find_library(
+            GLFW_LIBRARY
+            NAMES "glfw3.lib"
+            PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/bin"
+                  "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+        )
+    else()
+        find_library(
+            GLFW_LIBRARY
+            NAMES "glfw3dll.lib" "glfw3.lib"
+            PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/bin"
+                  "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+        )
+    endif()
+else()
+    if (WIN32)
+        if (USE_STATIC_LIBS)
+            find_library(
+                GLFW_LIBRARY
+                NAMES "libglfw3.a"
+                PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/bin"
+                      "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+            )
+        else()
+            find_library(
+                GLFW_LIBRARY
+                NAMES "libglfw3dll.a" "libglfw3.a"
+                PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/bin"
+                      "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+            )
+        endif()
+    else()
+        if (USE_STATIC_LIBS)
+            find_library(
+                GLFW_LIBRARY
+                NAMES "libglfw3.a"
+                PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/bin"
+                      "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+            )
+        else()
+            find_library(
+                GLFW_LIBRARY
+                NAMES "libglfw.so.3" "libglfw3.a"
+                PATHS "${CMAKE_SOURCE_DIR}/libs/GLFW/lib" "${CMAKE_SOURCE_DIR}/libs/GLFW/bin"
+                      "${CMAKE_SOURCE_DIR}/libs/GLFW/src" "${CMAKE_SOURCE_DIR}/libs/GLFW/build/src"
+            )
+        endif()
+    endif()
+endif()
+
+include("FindPackageHandleStandardArgs")
+find_package_handle_standard_args("GLFW" DEFAULT_MSG GLFW_LIBRARY GLFW_INCLUDE_DIR)
