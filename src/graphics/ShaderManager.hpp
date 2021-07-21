f73a9ce2a26b2cd0797fabdce6a4ed4d2793e6a7
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-21 09:57:59 -0400

    Added the header file for the shader manager class

---
+++ src/graphics/ShaderManager.hpp
@@ -0,0 +1,37 @@
+#ifndef DEF8B898_183A_498C_89FF_C2AD6C477FDF
+#define DEF8B898_183A_498C_89FF_C2AD6C477FDF
+
+#include <vector>
+
+// This is a special type of singleton class that just manages shaders, destroys them when you don't need them anymore.
+
+namespace Graphics {
+    class ShaderManager {
+    private:
+        // Vector of Shaders
+        std::vector<unsigned int> shaders;
+        
+        // The latest index (incremented everytime you add a shader)
+        unsigned int latestIndex = 0;
+        
+        // Made the constructors private so there can't be an instance
+        ShaderManager();
+        ShaderManager(ShaderManager& src);
+        ShaderManager(ShaderManager&& src);
+        
+    public:
+        // Returns the instance you need.
+        static ShaderManager& getInstance();
+        
+        // Add a shader object to the vector and return the index
+        unsigned int add(unsigned int shader);
+        
+        // Remove a shader object from the vector
+        void remove(unsigned int index);
+        
+        // Destroys all the shaders that hasn't been destroyed.
+        ~ShaderManager();
+    };
+}
+
+#endif /* DEF8B898_183A_498C_89FF_C2AD6C477FDF */
