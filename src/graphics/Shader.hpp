7e194b69d8b4793862292f4a0ea13d5df6b014ed
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-21 09:38:41 -0400

    Added the header file for the Shader class

---
+++ src/graphics/Shader.hpp
@@ -0,0 +1,39 @@
+#ifndef D0C2FD95_F1CF_46DB_ACFB_EC73D871C43A
+#define D0C2FD95_F1CF_46DB_ACFB_EC73D871C43A
+
+// The shader class, which represents a OpenGL Shader.
+
+namespace graphics {
+    class Shader {
+    private:
+        // The handle to the shader program
+        unsigned int program;
+        
+        // The index of the shader
+        unsigned int index;
+        
+        // Friend class ShaderManager
+        friend class ShaderManager;
+        
+    public:
+        // The default constructor
+        Shader();
+        
+        // Constructs a Shader object from a shader source
+        Shader(std::string_view path);
+        
+        // Constructs a Shader object from a vertex source and a fragment source
+        Shader(std::string_view vertexPath, std::string_view fragmentPath);
+        
+        // Use the program
+        void use();
+        
+        // Utility functions for setting uniforms
+        void setUniform(std::string_view name, int value);
+        
+        // Destroys the shader
+        void destroy();
+    };
+}
+
+#endif /* D0C2FD95_F1CF_46DB_ACFB_EC73D871C43A */
