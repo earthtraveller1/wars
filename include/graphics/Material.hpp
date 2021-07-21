7fbcbe5cc93da7a31cb917b874e5478ca833a3e1
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-21 14:59:53 -0400

    Created the header file for the material class

---
+++ include/graphics/Material.hpp
@@ -0,0 +1,25 @@
+#ifndef A2307F2D_7981_4658_A5F3_C2244BB71B37
+#define A2307F2D_7981_4658_A5F3_C2244BB71B37
+
+#include <graphics/Shader.hpp>
+
+namespace graphics {
+    // A Class that represents a specific material
+    class Material {
+    private:
+        // The shader
+        Shader shader;
+        
+    public:
+        // Default Constructor
+        Material();
+        
+        // Constructor specifying shader
+        Material(Shader shader);
+        
+        // Use the material
+        void use();
+    };
+}
+
+#endif /* A2307F2D_7981_4658_A5F3_C2244BB71B37 */
