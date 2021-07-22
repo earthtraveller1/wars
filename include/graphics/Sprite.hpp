4aa3649c54fe69f57b11f459d772271fe28e13d2
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-22 09:30:50 -0400

    Finished the header file for the sprite class

--- include/graphics/Sprite.hpp
+++ include/graphics/Sprite.hpp
@@ -1,6 +1,10 @@
 #ifndef C0D911E7_8ADB_4629_80C3_A1DEEC083B8E
 #define C0D911E7_8ADB_4629_80C3_A1DEEC083B8E
 
+#include <graphics/Vertex.hpp>
+#include <vector>
+#include <graphics/Material.hpp>
+
 namespace graphics {
     // A Class that represents a sprite. Really basic for now, since I don't have much special needs
     class Sprite {
@@ -10,9 +14,45 @@ namespace graphics {
         unsigned int vbo;
         unsigned int ebo;
         
+        // Material
+        Material material;
+        
+        // Move ownership from another object
+        void moveFrom(Sprite& src);
+        
+        // Destroy the sprite
+        void destroy();
+        
     public:
         // Default Constructor
         Sprite();
+        
+        // Construct a Object from a vector of vertices
+        Sprite(std::vector<Vertex> vertices);
+        
+        // Construct a Sprite from a vector of vertices and indices
+        Sprite(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
+        
+        // Copy constructor
+        Sprite(Sprite& src);
+        
+        // Copy assignment operator
+        Sprite& operator=(Sprite& rhs);
+        
+        // Move constructor
+        Sprite(Sprite&& src);
+        
+        // Move assignment operator
+        Sprite& operator=(Sprite&& rhs);
+        
+        // Render the sprite
+        void render();
+        
+        // Swap two sprites
+        static void swap(Sprite& a, Sprite& b);
+        
+        // Destructor
+        ~Sprite();
     };
 }
 
