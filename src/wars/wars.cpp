377daa13c448572ca80eccf1e7b1e5dd5940bc02
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-22 10:23:46 -0400

    Used the sprites class (it works)

--- src/wars/wars.cpp
+++ src/wars/wars.cpp
@@ -2,15 +2,51 @@
 #include <glad/glad.h>
 #include "Window.hpp"
 #include <graphics/basic-utils.hpp>
+#include <graphics/Sprite.hpp>
+#include <graphics/Material.hpp>
+#include <graphics/Shader.hpp>
+
+using graphics::Sprite;
+using graphics::Material;
+using graphics::Shader;
 
 int main() {
     std::cout << "[INFO]: Hello!" << std::endl;
     
     Window window(0.6, "", false);
     
+    Sprite sprite(
+        {
+            {
+                { 0.5, 0.5 },
+                { 1.0, 1.0 }
+            },
+            {
+                { 0.5, -0.5 },
+                { 1.0, 0.0 }
+            },
+            {
+                { -0.5, -0.5 },
+                { 0.0, 0.0 }
+            },
+            {
+                { -0.5, 0.5 },
+                { 0.0, 1.0 }
+            }
+        },
+        
+        {
+            0, 1, 2, 0, 3, 2
+        },
+        
+        Material(Shader("shaders/default.glsl"))
+    );
+    
     window.show();
     while (window.isOpen()) {
         bgu::clear();
+        
+        sprite.render();
         
         window.update();
     }
