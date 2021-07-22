995af116b51380ad1f4275a9769729380254373a
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-22 15:12:22 -0400

    Added the coordinate system.

--- src/wars/wars.cpp
+++ src/wars/wars.cpp
@@ -5,32 +5,36 @@
 #include <graphics/Mesh.hpp>
 #include <graphics/Material.hpp>
 #include <graphics/Shader.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 using graphics::Mesh;
 using graphics::Material;
 using graphics::Shader;
 
+const int WINDOW_WIDTH = 960;
+const int WINDOW_HEIGHT = 540;
+
 int main() {
     std::cout << "[INFO]: Hello!" << std::endl;
     
-    Window window(0.6, "", false);
+    Window window(WINDOW_WIDTH, WINDOW_HEIGHT, "", false);
     
     Mesh mesh(
         {
             {
-                { 0.5, 0.5 },
+                { 10.0, 10.0 },
                 { 1.0, 1.0 }
             },
             {
-                { 0.5, -0.5 },
-                { 1.0, 0.0 }
+                { 10.0, -10.0 },
+                { 10.0, 0.0 }
             },
             {
-                { -0.5, -0.5 },
+                { -10.0, -10.0 },
                 { 0.0, 0.0 }
             },
             {
-                { -0.5, 0.5 },
+                { -10.0, 10.0 },
                 { 0.0, 1.0 }
             }
         },
@@ -41,11 +45,21 @@ int main() {
     
     Material material(Shader("shaders/default.glsl"));
     
+    glm::mat4 model(1.0f);
+    
     window.show();
     while (window.isOpen()) {
         bgu::clear();
         
+        float windowWidth = static_cast<float>(window.getWidth());
+        float windowHeight = static_cast<float>(window.getHeight());
+        glm::mat4 projection = glm::ortho(-windowWidth, windowWidth, -windowHeight, windowHeight, -1.0f, 1.0f);
+        
         material.use();
+        
+        material.setUniform("model", model);
+        material.setUniform("projection", projection);
+        
         mesh.render();
         
         window.update();
