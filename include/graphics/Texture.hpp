131efff537bc2b6928006215c195cc087c22bdce
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-23 10:02:44 -0400

    Started on the texture class

---
+++ include/graphics/Texture.hpp
@@ -0,0 +1,22 @@
+#ifndef D41FEB62_166D_44F6_AF61_33D30907876C
+#define D41FEB62_166D_44F6_AF61_33D30907876C
+
+#include <string_view>
+
+namespace graphics {
+    // A Class which represents a texture. Very basic for now, with basic raii
+    class Texture {
+    private:
+        // The handle to the real texture object
+        int texture;
+        
+    public:
+        // Default Constructor
+        Texture();
+        
+        // Constructor from a file path
+        Texture(std::string_view imagePath);
+    };
+}
+
+#endif /* D41FEB62_166D_44F6_AF61_33D30907876C */
