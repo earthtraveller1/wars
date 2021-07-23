d0547892aaea70df5d428e54f87296829215c129
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-23 15:10:35 -0400

    Started on the implementation of the Texture Atlas class

---
+++ src/graphics/TextureAtlas.cpp
@@ -0,0 +1,47 @@
+#include <graphics/TextureAtlas.hpp>
+
+using graphics::TextureAtlas;
+
+void TextureAtlas::moveFrom(TextureAtlas& src) {
+    
+}
+
+void TextureAtlas::destroy() {
+    
+}
+
+TextureAtlas::TextureAtlas() {
+    
+}
+
+TextureAtlas::TextureAtlas(std::string_view imagePath) {
+    
+}
+
+TextureAtlas::TextureAtlas(TextureAtlas& src) {
+    
+}
+
+TextureAtlas& TextureAtlas::operator=(TextureAtlas rhs) {
+    
+}
+
+TextureAtlas::TextureAtlas(TextureAtlas&& src) {
+    
+}
+
+TextureAtlas& TextureAtlas::operator=(TextureAtlas&& rhs) {
+    
+}
+
+void TextureAtlas::bind() {
+    
+}
+
+void TextureAtlas::unbind() {
+    
+}
+
+TextureAtlas::~TextureAtlas() {
+    
+}
