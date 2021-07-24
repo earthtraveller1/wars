78e0d9d0124ad7c96b685cc2e84e371ea40f49be
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-24 15:45:16 -0400

    Placed the projection matrix into the public namespace

---
+++ include/graphics/ProjectionMatrix.hpp
@@ -0,0 +1,8 @@
+#ifndef BE98097E_ACFA_4E2D_9AEE_F3A5FBEC1737
+#define BE98097E_ACFA_4E2D_9AEE_F3A5FBEC1737
+
+namespace graphics {
+    extern glm::mat4 projectionMatrix;
+}
+
+#endif /* BE98097E_ACFA_4E2D_9AEE_F3A5FBEC1737 */
---
+++ src/graphics/ProjectionMatrix.cpp
@@ -0,0 +1,5 @@
+#include <glm/glm.hpp>
+
+#include <graphics/ProjectionMatrix.hpp>
+
+glm::mat4 graphics::projectionMatrix = glm::mat4(1.0f);
