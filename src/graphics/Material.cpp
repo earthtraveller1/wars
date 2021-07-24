a4b9191f5f03e95033f2d6bb10cfbc5089767992
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-24 15:53:28 -0400

    Set the projection matrix on construction.

--- src/graphics/Material.cpp
+++ src/graphics/Material.cpp
@@ -1,7 +1,9 @@
 #include <graphics/Material.hpp>
+#include <graphics/ProjectionMatrix.hpp>
 
 graphics::Material::Material(Shader& p_shader, TextureAtlas& p_atlas): shader(p_shader), textureAtlas(p_atlas) {
-    
+    shader.use();
+    shader.setUniform("projection", projectionMatrix);
 }
 
 void graphics::Material::use() {
