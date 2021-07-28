b24f4cc09b28bc699afbddfdf5154d2479fb27f0
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-28 15:40:58 -0400

    Started on the sprite class

---
+++ include/graphics/Sprite.hpp
@@ -0,0 +1,11 @@
+#ifndef DCF0C048_29F6_4EC8_8D7E_34707C8BC96E
+#define DCF0C048_29F6_4EC8_8D7E_34707C8BC96E
+
+namespace graphics {
+    // A Class to represent a sprite. Basic for now, add more features later on.
+    class Sprite {
+        
+    }
+}
+
+#endif /* DCF0C048_29F6_4EC8_8D7E_34707C8BC96E */
