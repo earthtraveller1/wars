5f9f581a1f37d22d881b81166fda25fc8259a6e9
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-28 16:03:07 -0400

    Added semicolon

--- include/graphics/Sprite.hpp
+++ include/graphics/Sprite.hpp
@@ -5,7 +5,7 @@ namespace graphics {
     // A Class to represent a sprite. Basic for now, add more features later on.
     class Sprite {
         
-    }
+    };
 }
 
 #endif /* DCF0C048_29F6_4EC8_8D7E_34707C8BC96E */
