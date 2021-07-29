3f6469a086d724ad1925e051223555a194fc6754
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-29 09:42:15 -0400

    Completed the basic part of the Sprite class

--- include/graphics/Sprite.hpp
+++ include/graphics/Sprite.hpp
@@ -1,10 +1,37 @@
 #ifndef DCF0C048_29F6_4EC8_8D7E_34707C8BC96E
 #define DCF0C048_29F6_4EC8_8D7E_34707C8BC96E
 
+#include <glm/mat4x4.hpp>
+
 namespace graphics {
+    class Mesh;
+    
+    class Material;
+    
     // A Class to represent a sprite. Basic for now, add more features later on.
     class Sprite {
+    private:
+        // The Mesh of the sprite (typically a rectangle)
+        Mesh& mesh;
         
+        // The Material of the sprite
+        Material& material;
+        
+        // The model matrix
+        glm::mat4 modelMatrix;
+        
+    public:
+        // Constructor
+        Sprite(Mesh& mesh, Material& material);
+        
+        // Draw the sprite
+        void draw();
+        
+        // Move the sprite
+        void move(int x, int y);
+        
+        // Turn the sprite
+        void turn(double degrees);
     };
 }
 
