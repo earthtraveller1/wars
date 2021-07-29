94b567dc3191ee4564090475bbbea46e5308d7d5
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-29 10:03:56 -0400

    Moved the resources to a separate header file.

---
+++ src/wars/Resources.hpp
@@ -0,0 +1,29 @@
+#ifndef E7988B6C_E6C4_4EDD_9DAC_7C0AAB9EDC8A
+#define E7988B6C_E6C4_4EDD_9DAC_7C0AAB9EDC8A
+
+namespace graphics {
+    class Mesh;
+    class Material;
+    class TextureAtlas;
+    class Shader;
+}
+
+namespace Resources {
+    namespace Meshes {
+        Mesh* square;
+    }
+    
+    namespace Shaders {
+        Shader* test;
+    }
+    
+    namespace TextureAtlases {
+        TextureAtlas* test;
+    }
+    
+    namespace Materials {
+        Material* test;
+    }
+}
+
+#endif /* E7988B6C_E6C4_4EDD_9DAC_7C0AAB9EDC8A */
