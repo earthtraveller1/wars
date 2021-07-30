40fbba0d50b9061120894eb85d2f4a8a0f0ac743
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-30 11:20:32 -0400

    Implemented the prepare Meshes function

---
+++ src/wars/Resources/prepareMeshes.cpp
@@ -0,0 +1,32 @@
+#include <graphics/Mesh.hpp>
+
+#include "../Resources.hpp"
+
+using namespace Resources::Meshes;
+using graphics::Mesh;
+
+void Resources::prepareMeshes() {
+    test = new Mesh(
+        {
+            {
+                { 100.0, 100.0 },
+                { 1.0, 1.0 }
+            },
+            {
+                { 100.0, -100.0 },
+                { 1.0, 0.0 }
+            },
+            {
+                { -100.0, -100.0 },
+                { 0.0, 0.0 }
+            },
+            {
+                { -100.0, 100.0 },
+                { 0.0, 1.0 }
+            }
+        },
+        {
+            0, 1, 2, 0, 3, 2
+        }
+    );
+}
