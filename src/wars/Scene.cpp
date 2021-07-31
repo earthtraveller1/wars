69b99355b904c5c5e73e9666896aba2d84510bd5
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-31 15:33:46 -0400

    Added the scene class.

---
+++ src/wars/Scene.cpp
@@ -0,0 +1,24 @@
+#include <graphics/Sprite.hpp>
+#include <vector>
+
+#include "Scene.hpp"
+
+using graphics::Sprite;
+
+Scene::Scene(std::vector<Sprite>& p_sprites): sprites(p_sprites) {
+    
+}
+
+void Scene::render() {
+    for (std::vector<Sprite>::iterator i = sprites.begin(); i != sprites.end(); i++) {
+        i->draw();
+    }
+}
+
+void Scene::setActive(Scene& newActive) {
+    active = &newActive;
+}
+
+void Scene::renderActive() {
+    active->render();
+}
---
+++ src/wars/Scene.hpp
@@ -0,0 +1,36 @@
+#ifndef AE31E619_96C8_4000_8724_D323B6A6BDB4
+#define AE31E619_96C8_4000_8724_D323B6A6BDB4
+
+namespace std {
+    template<typename T, typename alloc>
+    class vector;
+}
+
+namespace graphics {
+    class Sprite;
+}
+
+class Scene {
+private:
+    std::vector<graphics::Sprite> sprites;
+    
+    // The scene that is currently active
+    static Scene* active = nullptr;
+public:
+    // Default Constructor, initializes an empty vector
+    Scene() = default;
+    
+    // Construct from a vector of sprites
+    Scene(std::vector<graphics::Sprite>& sprites);
+    
+    // Render the scene
+    void render();
+    
+    // Set the active scene
+    static void setActive(Scene& newActive);
+    
+    // Render the active scene
+    static void renderActive();
+}
+
+#endif /* AE31E619_96C8_4000_8724_D323B6A6BDB4 */
