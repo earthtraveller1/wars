5aa3b7fce59170fe55a324a257c15c4c6728bd41
Liangyue Zhao <liangyuez1@outlook.com>
2021-07-31 15:36:58 -0400

    Implemented the lvalue constructor

--- src/wars/Scene.cpp
+++ src/wars/Scene.cpp
@@ -9,6 +9,10 @@ Scene::Scene(std::vector<Sprite>& p_sprites): sprites(p_sprites) {
     
 }
 
+Scene::Scene(std::vector<Sprite>&& p_sprites): sprites(p_sprites) {
+    
+}
+
 void Scene::render() {
     for (std::vector<Sprite>::iterator i = sprites.begin(); i != sprites.end(); i++) {
         i->draw();
