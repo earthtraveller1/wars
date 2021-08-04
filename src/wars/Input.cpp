d27e0278ade5612c883eb524e12de2b786233eed
Liangyue Zhao <liangyuez1@outlook.com>
2021-08-04 15:35:46 -0400

    Added Mouse input functionalities.

--- src/wars/Input.cpp
+++ src/wars/Input.cpp
@@ -12,3 +12,32 @@ bool Input::isKeyDown(int key) {
 void Input::setWindow(Window& window) {
     ::window = window.window;
 }
+
+int Input::getMouseX() {
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    
+    return xpos;
+}
+
+int Input::getMouseY() {
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    
+    return ypos;
+}
+
+glm::vec2 Input::getMousePos() {
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    
+    return { xpos, ypos };
+}
+
+bool Input::isMouseButtonDown(int button) {
+    return (glfwGetMouseButton(window, button) == GLFW_PRESS);
+}
+
+bool Input::isMouseButtonReleased(int button) {
+    return (glfwGetMouseButton(window, button) == GLFW_RELEASE);
+}
--- src/wars/Input.hpp
+++ src/wars/Input.hpp
@@ -1,6 +1,8 @@
 #ifndef D7CDE22F_464B_42CB_A6A1_D57D4724A1EA
 #define D7CDE22F_464B_42CB_A6A1_D57D4724A1EA
 
+#include <glm/vec2.hpp>
+
 class Window;
 
 class Input {
@@ -10,6 +12,21 @@ public:
     
     // Set the window to check for input from
     static void setWindow(Window& window);
+    
+    // Get the cursor x
+    static int getMouseX();
+    
+    // Get the cursor y
+    static int getMouseY();
+    
+    // Get the cursor position
+    glm::vec2 getMousePos();
+    
+    // check whether the mouse is clicked or not
+    static bool isMouseButtonDown(int button);
+    
+    // Check whether the mouse is released or not
+    static bool isMouseButtonReleased(int button);
 };
 
 #endif /* D7CDE22F_464B_42CB_A6A1_D57D4724A1EA */
