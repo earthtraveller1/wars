6302dd863ae067da3974e86d014bff5eb67ce585
Liangyue Zhao <liangyuez1@outlook.com>
2021-08-12 09:31:57 -0400

    Fixed the move function to take the direction into account

--- src/wars/sprite-types/Player.cpp
+++ src/wars/sprite-types/Player.cpp
@@ -8,5 +8,9 @@ Player::Player(): Sprite(*Resources::Meshes::player), CollisionBox(91.5, -91.5, 
 
 void Player::move(double x, double y) {
     Sprite::move(x, y);
-    CollisionBox::move(x, y);
+    if (dir == graphics::Direction::LEFT) {
+        CollisionBox::move(-x, y);
+    } else {
+        CollisionBox::move(x, y);
+    }
 }
