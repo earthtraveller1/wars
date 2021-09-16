c837884741cfc21958bb8e49668ac11535341f05
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-16 18:57:33 -0400

    Added some spaces to make it more readable

--- src/engine/events/key_press_event.cpp
+++ src/engine/events/key_press_event.cpp
@@ -9,10 +9,16 @@ key_press_event::key_press_event(int32_t key_code): m_key_code(key_code)
     
 }
 
+
+
+
 int32_t key_press_event::get_key_code()
 {
     return m_key_code;
 }
+
+
+
 
 key_press_event::~key_press_event()
 {
