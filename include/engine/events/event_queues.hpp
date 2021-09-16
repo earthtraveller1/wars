5fbb4c4db854794d4f035afedc695adc9bbb34ac
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-16 18:31:50 -0400

    added a basic event queue.

---
+++ include/engine/events/event_queues.hpp
@@ -0,0 +1,16 @@
+#ifndef D811731D_568C_457C_A68E_4FC2E9217C87
+#define D811731D_568C_457C_A68E_4FC2E9217C87
+
+#include <queue>
+
+namespace wars
+{
+    class key_press_event;
+    
+    namespace event_queues
+    {
+        extern std::queue<key_press_event> key_press;
+    }
+}
+
+#endif /* D811731D_568C_457C_A68E_4FC2E9217C87 */
---
+++ src/engine/events/event_queues.cpp
@@ -0,0 +1,5 @@
+#include <engine/events/key_press_event.hpp>
+
+#include <engine/events/event_queues.hpp>
+
+std::queue<wars::key_press_event> wars::event_queues::key_press;
