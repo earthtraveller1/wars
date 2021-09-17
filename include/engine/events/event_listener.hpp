bac903983aeb7ed2377e5e396885cf424ac03978
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-17 18:39:15 -0400

    Added a basic event listener class.

---
+++ include/engine/events/event_listener.hpp
@@ -0,0 +1,17 @@
+#ifndef EB383F48_FF8D_4C7C_B0B7_BEA615AF9270
+#define EB383F48_FF8D_4C7C_B0B7_BEA615AF9270
+
+#pragma once
+
+#include <engine/events/key_press_event.hpp>
+
+namespace wars::engine::events
+{
+    class event_listener
+    {
+    public:
+        virtual bool invoke_key_press(key_press_event& event) { return false; }
+    };
+}
+
+#endif /* EB383F48_FF8D_4C7C_B0B7_BEA615AF9270 */
