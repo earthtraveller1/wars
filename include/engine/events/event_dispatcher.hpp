bd8ce91b93057fdbd070c2b4849327c8e9bcda4f
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-17 18:36:23 -0400

    Started on the event dispatcher class

---
+++ include/engine/events/event_dispatcher.hpp
@@ -0,0 +1,16 @@
+#ifndef D7DD1C83_E713_4ECD_9AFA_B011189C92CF
+#define D7DD1C83_E713_4ECD_9AFA_B011189C92CF
+
+#include <pch.hpp>
+#include <engine/events/key_press_event.hpp>
+
+namespace wars::engine::events
+{
+    class event_dispatcher
+    {
+    public:
+        event_dispatcher() = default;
+    }
+}
+
+#endif /* D7DD1C83_E713_4ECD_9AFA_B011189C92CF */
