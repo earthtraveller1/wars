76a126d1d0711a3489827d42a78868427878cabe
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-17 17:59:05 -0400

    Removed the event system.

--- src/engine/events/event_listener.cpp
+++
@@ -1,8 +0,0 @@
-#include <engine/events/event_listener.hpp>
-
-using namespace wars;
-
-bool event_listener::invoke_key_press(key_press_event& event) const 
-{
-    return false;
-}
--- src/engine/events/event_queues.cpp
+++
@@ -1,5 +0,0 @@
-#include <engine/events/key_press_event.hpp>
-
-#include <engine/events/event_queues.hpp>
-
-std::queue<wars::key_press_event> wars::event_queues::key_press;
