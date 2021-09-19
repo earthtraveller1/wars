708365dbd595319dc2d0dcb74c51488558731f4e
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-18 21:37:20 -0400

    Set the background color to be green.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <pch.hpp>
 
+#include <glad/glad.h>
 #include <Window.hpp>
 
 using wars::Window;
@@ -10,8 +11,12 @@ int main()
     
     window.show();
     
+    glClearColor(0.0f, 1.0f, 0.0, 1.0f);
+    
     while (window.isOpen()) 
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
         window.update();
     }
     
