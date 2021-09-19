0edd9b6578ae51bd95aca883a55f57bedbbbabe7
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-19 19:03:08 -0400

    Processed command line options.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,32 @@
 
 #include <glad/glad.h>
 #include <Game.hpp>
+#include <GameOptions.hpp>
 
 using wars::Window;
 
-int main()
+int main(int argc, char** argv)
 {
+    // Put the command line arguments into a vector so that it's easier to deal
+    // with.
+    std::vector<std::string> commandLineArguments(argc);
+    for (uint16_t i = 0; i < argc; i++)
+    {
+        commandLineArguments[i] = argv[i];
+    }
+    
+    // Process the command line arguments
+    for (std::vector<std::string>::iterator argument = commandLineArguments.begin(); argument != commandLineArguments.end(); argument++)
+    {
+        if (*argument == "--asset-location")
+        {
+            wars::GameOptions::assetLocation = *(argument + 1);
+        } else
+        {
+            wars::GameOptions::assetLocation = *argument;
+        }
+    }
+    
     try
     {
         wars::Game game;
