97c28041a8ac34f9ce4c2b087f96ba19ad9bdfcf
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-20 17:03:57 -0400

    Created a stub for the Sprite class.

---
+++ include/graphics/Sprite.hpp
@@ -0,0 +1,12 @@
+#ifndef D1B01983_88B3_4F44_851C_894D2F00BFF7
+#define D1B01983_88B3_4F44_851C_894D2F00BFF7
+
+namespace wars::graphics
+{
+    class Sprite
+    {
+        
+    }
+}
+
+#endif /* D1B01983_88B3_4F44_851C_894D2F00BFF7 */
