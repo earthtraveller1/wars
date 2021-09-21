5bf7abb970989e1c0cff8ab01f514cc5d264dadf
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-21 16:53:39 -0400

    Finished the Matrix4 class.

--- include/math/Matrix4.hpp
+++ include/math/Matrix4.hpp
@@ -10,15 +10,140 @@ namespace wars::math
         // Initializes an identity matrix.
         Matrix4(T value = 1)
         {
-            m_matrix[(1 * 1)] = value;
-            m_matrix[(2 * 2)] = value;
-            m_matrix[(3 * 3)] = value;
-            m_matrix[(4 * 4)] = value;
+            m_matrix[0][0] = value;
+            m_matrix[1][1] = value;
+            m_matrix[2][2] = value;
+            m_matrix[3][3] = value;
+        }
+        
+        
+        
+        
+        // Matrix-matrix multiplication
+        Matrix4<T> operator+(Matrix4<T>& b)
+        {
+            return
+            {
+                {
+                    {
+                        m_matrix[0][0] * b.m_matrix[0][0] +
+                        m_matrix[1][0] * b.m_matrix[0][1] +
+                        m_matrix[2][0] * b.m_matrix[0][2] +
+                        m_matrix[3][0] * b.m_matrix[0][3],
+                        
+                        m_matrix[0][0] * b.m_matrix[1][0] +
+                        m_matrix[1][0] * b.m_matrix[1][1] +
+                        m_matrix[2][0] * b.m_matrix[1][2] +
+                        m_matrix[3][0] * b.m_matrix[1][3],
+                        
+                        m_matrix[0][0] * b.m_matrix[2][0] +
+                        m_matrix[1][0] * b.m_matrix[2][1] +
+                        m_matrix[2][0] * b.m_matrix[2][2] +
+                        m_matrix[3][0] * b.m_matrix[2][3],
+                        
+                        m_matrix[0][0] * b.m_matrix[3][0] +
+                        m_matrix[1][0] * b.m_matrix[3][1] +
+                        m_matrix[2][0] * b.m_matrix[3][2] +
+                        m_matrix[3][0] * b.m_matrix[3][3],
+                    },
+                    {
+                        m_matrix[0][1] * b.m_matrix[0][0] +
+                        m_matrix[1][1] * b.m_matrix[0][1] +
+                        m_matrix[2][1] * b.m_matrix[0][2] +
+                        m_matrix[3][1] * b.m_matrix[0][3],
+                        
+                        m_matrix[0][1] * b.m_matrix[1][0] +
+                        m_matrix[1][1] * b.m_matrix[1][1] +
+                        m_matrix[2][1] * b.m_matrix[1][2] +
+                        m_matrix[3][1] * b.m_matrix[1][3],
+                        
+                        m_matrix[0][1] * b.m_matrix[2][0] +
+                        m_matrix[1][1] * b.m_matrix[2][1] +
+                        m_matrix[2][1] * b.m_matrix[2][2] +
+                        m_matrix[3][1] * b.m_matrix[2][3],
+                        
+                        m_matrix[0][1] * b.m_matrix[3][0] +
+                        m_matrix[1][1] * b.m_matrix[3][1] +
+                        m_matrix[2][1] * b.m_matrix[3][2] +
+                        m_matrix[3][1] * b.m_matrix[3][3],
+                    },
+                    {
+                        m_matrix[0][2] * b.m_matrix[0][0] +
+                        m_matrix[1][2] * b.m_matrix[0][1] +
+                        m_matrix[2][2] * b.m_matrix[0][2] +
+                        m_matrix[3][2] * b.m_matrix[0][3],
+                        
+                        m_matrix[0][2] * b.m_matrix[1][0] +
+                        m_matrix[1][2] * b.m_matrix[1][1] +
+                        m_matrix[2][2] * b.m_matrix[1][2] +
+                        m_matrix[3][2] * b.m_matrix[1][3],
+                        
+                        m_matrix[0][2] * b.m_matrix[2][0] +
+                        m_matrix[1][2] * b.m_matrix[2][1] +
+                        m_matrix[2][2] * b.m_matrix[2][2] +
+                        m_matrix[3][2] * b.m_matrix[2][3],
+                        
+                        m_matrix[0][2] * b.m_matrix[3][0] +
+                        m_matrix[1][2] * b.m_matrix[3][1] +
+                        m_matrix[2][2] * b.m_matrix[3][2] +
+                        m_matrix[3][2] * b.m_matrix[3][3],
+                    },
+                    {
+                        m_matrix[0][3] * b.m_matrix[0][0] +
+                        m_matrix[1][3] * b.m_matrix[0][1] +
+                        m_matrix[2][3] * b.m_matrix[0][2] +
+                        m_matrix[3][3] * b.m_matrix[0][3],
+                        
+                        m_matrix[0][3] * b.m_matrix[1][0] +
+                        m_matrix[1][3] * b.m_matrix[1][1] +
+                        m_matrix[2][3] * b.m_matrix[1][2] +
+                        m_matrix[3][3] * b.m_matrix[1][3],
+                        
+                        m_matrix[0][3] * b.m_matrix[2][0] +
+                        m_matrix[1][3] * b.m_matrix[2][1] +
+                        m_matrix[2][3] * b.m_matrix[2][2] +
+                        m_matrix[3][3] * b.m_matrix[2][3],
+                        
+                        m_matrix[0][3] * b.m_matrix[3][0] +
+                        m_matrix[1][3] * b.m_matrix[3][1] +
+                        m_matrix[2][3] * b.m_matrix[3][2] +
+                        m_matrix[3][3] * b.m_matrix[3][3],
+                    },
+                }
+            };
+        }
+        
+        
+        
+        // Returns the matrix in the format that OpenGL can work with
+        T* data()
+        {
+            return {
+                m_matrix[0][0],
+                m_matrix[0][1],
+                m_matrix[0][2],
+                m_matrix[0][3],
+                
+                m_matrix[1][0],
+                m_matrix[1][1],
+                m_matrix[1][2],
+                m_matrix[1][3],
+                
+                m_matrix[2][0],
+                m_matrix[2][1],
+                m_matrix[2][2],
+                m_matrix[2][3],
+                
+                m_matrix[3][0],
+                m_matrix[3][1],
+                m_matrix[3][2],
+                m_matrix[3][3],
+            };
         }
         
     private:
         // The actual matrix
-        T m_matrix[16];
+        T m_matrix[4][4];
     };
 }
 
