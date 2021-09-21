4423b0b640c183cbf7679c9dc308ece6c4d334d1
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-21 16:58:33 -0400

    Started on the Math class.

---
+++ include/Math.hpp
@@ -0,0 +1,22 @@
+#ifndef B0F9CEAB_54FF_4726_A816_ADDC48B90C0C
+#define B0F9CEAB_54FF_4726_A816_ADDC48B90C0C
+
+#include <math/Matrix4.hpp>
+
+namespace wars
+{
+    // A class containing common Math utility functions that aren't provided by
+    // the C or C++ standard library, which are mainly matrix transformations.
+    class Math
+    {
+    public:
+        // Returns a scale matrix
+        template<typename T>
+        math::Matrix4<T> scale()
+        {
+            
+        }
+    }
+}
+
+#endif /* B0F9CEAB_54FF_4726_A816_ADDC48B90C0C */
