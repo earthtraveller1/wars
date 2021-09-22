d0e5cc896b041f8954a23e4d632e1c50b13e05f7
Liangyue Zhao <liangyuez1@outlook.com>
2021-09-22 17:52:50 -0400

    Created a struct to represent a vertex.

---
+++ include/graphics/Vertex.hpp
@@ -0,0 +1,15 @@
+#ifndef DACAB8AF_A20D_4E77_B2FF_7475B60AE8E2
+#define DACAB8AF_A20D_4E77_B2FF_7475B60AE8E2
+
+#include <math/Vector2.hpp>
+
+namespace wars::graphics
+{
+    struct Vertex
+    {
+        math::Vector2<double> pos;
+        math::Vector2<double> uv;
+    };
+}
+
+#endif /* DACAB8AF_A20D_4E77_B2FF_7475B60AE8E2 */
