5630b71a095c93de0ce5a25778e92383c113d860
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-11-13 18:02:56 -0500

    Added set uniform functions.

--- include/graphics/Shader.hpp
+++ include/graphics/Shader.hpp
@@ -1,6 +1,8 @@
 #ifndef A7CC729D_FF40_4283_AC11_9FC988D192E9
 #define A7CC729D_FF40_4283_AC11_9FC988D192E9
 
+#include <pch.hpp>
+
 namespace wars::graphics
 {
     // A class built to handle a OpenGL shader. It is very basic for now. I 
@@ -19,6 +21,13 @@ namespace wars::graphics
         
         // Constructor from a string containing the source of the shader
         Shader(std::string_view source);
+        
+        // Set a uniform that's an integer
+        void setUniform(std::string_view name, int32_t value) { glUniform1i(glGetUniformLocation(m_shader, name.data()), value); }
+        
+        // Set a uniform that's a matrix
+        template<typename T>
+        void setUniform(std::string_view name, math::Matrix4<T> value) { glUniformMatrix4fv(glGetUniformLocation(m_shader, name.data()), 1, GL_FALSE, value.data()); }
         
         // The handle to the actual shader program
         unsigned int m_shader;
