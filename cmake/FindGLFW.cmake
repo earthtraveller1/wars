08c2e476b5a9cb0ef5685fe65f5da2071ea25d6f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2021-11-28 17:53:14 -0500

    Fixed the casing.

--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -1,7 +1,7 @@
 find_path(
     GLFW_INCLUDE_DIR
     NAMES "GLFW/glfw3.h"
-    PATHS "${CMAKE_SOURCE_DIR}/deps/GLFW/include"
+    PATHS "${CMAKE_SOURCE_DIR}/deps/glfw/include"
     DOC "Absolute path to GLFW's include directory."
 )
 
@@ -10,22 +10,22 @@ if (WIN32)
         find_library(
             GLFW_LIBRARY
             NAMES "glfw3.lib" "glfw3dll.lib"
-            PATHS "${CMAKE_SOURCE_DIR}/deps/GLFW/lib"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/Debug/src"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/Release/src"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/build/Debug/src"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/build/Release/src"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/build/src/Debug"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/build/src/Release"
+            PATHS "${CMAKE_SOURCE_DIR}/deps/glfw/lib"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/Debug/src"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/Release/src"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/build/Debug/src"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/build/Release/src"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/build/src/Debug"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/build/src/Release"
             DOC "Path to GLFW's library."
         )
     else()
         find_library(
             GLFW_LIBRARY
             NAMES "libglfw3.a" "libglfw3dll.a"
-            PATHS "${CMAKE_SOURCE_DIR}/deps/GLFW/lib"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/src"
-                  "${CMAKE_SOURCE_DIR}/deps/GLFW/build/src"
+            PATHS "${CMAKE_SOURCE_DIR}/deps/glfw/lib"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/src"
+                  "${CMAKE_SOURCE_DIR}/deps/glfw/build/src"
             DOC "Absolute path to GLFW's library."
         )
     endif()
@@ -33,9 +33,9 @@ elseif(UNIX AND NOT APPLE)
     find_library(
         GLFW_LIBRARY
         NAMES "libglfw3.a" "libglfw.so.3"
-        PATHS "${CMAKE_SOURCE_DIR}/deps/GLFW/lib"
-              "${CMAKE_SOURCE_DIR}/deps/GLFW/src"
-              "${CMAKE_SOURCE_DIR}/deps/GLFW/build/src"
+        PATHS "${CMAKE_SOURCE_DIR}/deps/glfw/lib"
+              "${CMAKE_SOURCE_DIR}/deps/glfw/src"
+              "${CMAKE_SOURCE_DIR}/deps/glfw/build/src"
         DOC "Absolute path to GLFW's library."
     )
 endif()
