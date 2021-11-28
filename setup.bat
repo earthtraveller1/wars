8960de78b6dbdbbd1dd24984c4235eee2e3dfdee
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2021-11-28 17:39:10 -0500

    Fixed the install script for windows a little.

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
-cmake -S deps/glfw -B deps/glfw/build
-msbuild deps/glfw/build/GLFW.sln
+cmake -S deps/glfw -B deps/glfw/build -D GLFW_BUILD_EXAMPLES=false -D GLFW_BUILD_TESTS=false
+cmake --build deps/glfw/build --target Release
 
 cmake -S . -B build
 cmake --build build --target Release
