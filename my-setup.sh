bd245f700daab8c002875bbff91f1202cc9bb0c8
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2021-11-28 17:57:04 -0500

    Removed unused flags.

--- my-setup.sh
+++ my-setup.sh
@@ -1,4 +1,4 @@
-cmake -S deps/glfw -B deps/glfw/build -D GLFW_BUILD_EXAMPLES=false -D GLFW_BUILD_TESTS=false -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -G Ninja
+cmake -S deps/glfw -B deps/glfw/build -D GLFW_BUILD_EXAMPLES=false -D GLFW_BUILD_TESTS=false -D CMAKE_C_COMPILER=clang -G Ninja
 cmake --build deps/glfw/build
 
 cmake -S . -B build -D GLFW_BUILD_TESTS=false -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -G Ninja
