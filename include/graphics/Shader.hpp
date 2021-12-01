d8fb1b88fbb8e863e95d17a2ff96cd5dc197f026
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2021-12-01 18:04:11 -0500

    Made the uniform setters public.

--- include/graphics/Shader.hpp
+++ include/graphics/Shader.hpp
@@ -15,19 +15,22 @@ namespace wars::graphics
         // the constructors will be private
         friend class Shaders;
         
-    private:
-        // Default constructor
-        Shader() = default;
-        
-        // Constructor from a string containing the source of the shader
-        Shader(std::string_view source);
-        
         // Set a uniform that's an integer
         void setUniform(std::string_view name, int32_t value) { glUniform1i(glGetUniformLocation(m_shader, name.data()), value); }
         
         // Set a uniform that's a matrix
         template<typename T>
         void setUniform(std::string_view name, math::Matrix4<T> value) { glUniformMatrix4fv(glGetUniformLocation(m_shader, name.data()), 1, GL_FALSE, value.data()); }
+        
+        // Set a uniform that's a 4 valued vector
+        void setUniform(std::string_view name, float x, float y, float z, float w) { glUniform4f(glGetUniformLocation(m_shader, name.data()), x, y, z, w); }
+        
+    private:
+        // Default constructor
+        Shader() = default;
+        
+        // Constructor from a string containing the source of the shader
+        Shader(std::string_view source);
         
         // The handle to the actual shader program
         unsigned int m_shader;
