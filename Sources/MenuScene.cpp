3a190809cf051cb4acf52a598fc5168043a82eb5
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-16 15:33:35 -0500

    Set the background color to green.

--- Sources/MenuScene.cpp
+++ Sources/MenuScene.cpp
@@ -1,8 +1,12 @@
+#include <scp/graphics/opengl.hpp>
+
 #include "MenuScene.hpp"
+
+namespace opengl = scp::graphics::opengl;
 
 MenuScene::MenuScene()
 {
-    
+    opengl::setClearColor(0.0f, 1.0f, 0.0f, 1.0f);
 }
 
 void MenuScene::onUpdate(double p_deltaTime)
