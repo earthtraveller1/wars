a85300d08919a441a35222479a04b3ab0c9ed32f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-18 11:20:13 -0500

    Made the button flash when hovered over top.

--- Sources/MenuScene.hpp
+++ Sources/MenuScene.hpp
@@ -3,16 +3,28 @@
 
 #include <scp/Scene.hpp>
 
+#include <scp/graphics/Renderer2D.hpp>
+#include <scp/graphics/Sprite.hpp>
+
+#include <scp/ui/Button.hpp>
+
 class MenuScene: public scp::Scene
 {
 public:
-    MenuScene();
+    MenuScene(scp::graphics::Renderer2D& renderer);
     
     void onUpdate(double deltaTime) override;
     
     void render() override;
     
     ~MenuScene() override;
+private:
+    // The renderer
+    scp::graphics::Renderer2D& m_renderer;
+    
+    // The play button
+    scp::graphics::Sprite m_playButtonSprite;
+    scp::ui::Button m_playButtonHandler;
 };
 
 #endif /* B84FAA61_C9C7_4546_901C_4F06F96A2EFA */
