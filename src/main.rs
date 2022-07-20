c740838165508d1726cb3c5056b41c2ee15c6513
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-07-20 11:11:07 -0400

    Used the Window class.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use wars::Window;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new(1280, 720, "Wars");
+    
+    window.show();
+    
+    while window.is_open() {
+        window.update();
+    }
 }
