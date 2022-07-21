f31ed5fddec7fec3471e28d33f195be9d9adab16
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-07-21 10:55:24 -0400

    Implemented Clone for the vectors.

--- src/math.rs
+++ src/math.rs
@@ -1,13 +1,28 @@
 #[repr(C)]
-pub struct Vector2<T> {
+pub struct Vector2<T: Copy> {
     pub x: T,
-    pub y: T
+    pub y: T,
+}
+
+impl<T: Copy> Clone for Vector2<T> {
+    fn clone(&self) -> Self {
+        return Vector2 {
+            x: self.x,
+            y: self.y,
+        };
+    }
 }
 
 #[repr(C)]
-pub struct Vector4<T> {
+pub struct Vector4<T: Copy> {
     pub x: T,
     pub y: T,
     pub z: T,
-    pub w: T
+    pub w: T,
+}
+
+impl<T: Copy> Clone for Vector4<T> {
+    fn clone(&self) -> Self {
+        return Vector4 { x: self.x, y: self.y, z: self.z, w: self.w };
+    }
 }
