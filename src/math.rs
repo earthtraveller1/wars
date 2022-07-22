61e7717978b461aa8b289951b22c12c37d1de60d
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-07-22 15:39:55 -0400

    Fixed the matrix not passing over to the GPU side.

--- src/math.rs
+++ src/math.rs
@@ -29,35 +29,35 @@ impl<T: Copy> Clone for Vector4<T> {
 
 #[repr(C)]
 pub struct Matrix4<T: Copy> {
-    matrix: [[T; 4]; 4]
+    matrix: [T; 16]
 }
 
 impl <T: Copy> Matrix4<T> {
     pub fn new(a: T, b: T) -> Matrix4<T> {
-        let mut matrix = [[a; 4]; 4];
-        matrix[0][0] = b;
-        matrix[1][1] = b;
-        matrix[2][2] = b;
-        matrix[3][3] = b;
+        let mut matrix = [a; 16];
+        matrix[0 * 4 + 0] = b;
+        matrix[1 * 4 + 1] = b;
+        matrix[2 * 4 + 2] = b;
+        matrix[3 * 4 + 3] = b;
         
         return Matrix4 { matrix };
     }
     
     pub fn as_ptr(&self) -> *const T {
-        return &(self.matrix[0][0]);
+        return self.matrix.as_ptr();
     }
 }
 
 pub fn orthographic(left: f32, right: f32, top: f32, bottom: f32, far: f32, near: f32) -> Matrix4<f32> {
     let mut matrix = Matrix4::<f32>::new(0.0, 1.0);
     
-    matrix.matrix[0][0] = 2.0 / (right - left);
-    matrix.matrix[1][1] = 2.0 / (top - bottom);
-    matrix.matrix[2][2] = -2.0 / (far - near);
+    matrix.matrix[0 * 4 + 0] = 2.0 / (right - left);
+    matrix.matrix[1 * 4 + 1] = 2.0 / (top - bottom);
+    matrix.matrix[2 * 4 + 2] = -2.0 / (far - near);
     
-    matrix.matrix[0][3] = -((right + left) / (right - left));
-    matrix.matrix[1][3] = -((top + bottom) / (top - bottom));
-    matrix.matrix[2][3] = -((far + near) / (far - near));
+    matrix.matrix[0 * 4 + 3] = -((right + left) / (right - left));
+    matrix.matrix[1 * 4 + 3] = -((top + bottom) / (top - bottom));
+    matrix.matrix[2 * 4 + 3] = -((far + near) / (far - near));
     
     return matrix;
 }
