f18f12fa4d4c0504ed93d2de7486a67f1e1992e8
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-07-22 20:39:42 -0400

    Made the menu scene the starting scene.

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ impl Game {
     pub fn new() -> Game {
         let mut window = Window::new(1280, 720, "Wars");
 
-        let scene_manager = SceneManager::new(Box::new(scenes::DummyScene::new()));
+        let scene_manager = SceneManager::new(Box::new(scenes::MenuScene::new()));
 
         window.show();
 
